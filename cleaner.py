36434ca6742d99607463e5881f6cda75342bd1e3
matthino <matthino@gmail.com>
2015-11-30 20:44:28 -0800

    Finished up cleanNT function. Added helper functions to format output nicely. More comments. Added new files to be tracked.

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,12 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
+
+####################################
+# Computational Biology Project Code
+#
+# @authors: Matt Hino, Steven Lind
+# @authors: Julia Meng, Shaun Stice
+# @version: November 30, 2015
+####################################
 
 # Given FASTA formatted sequences in a .nt file (inputFile),
 # this function cleans the sequences by trimming
@@ -10,69 +18,95 @@
 # ex.
 # Prior to clean: NNNNANNCCTTGNAATCTCTAANANNANNN
 # After cleaning: CCTTGNAATCTCTAA
-def cleanNT(inputFile):
-    # open the file for reading
-    inputFile = open(inputFile, "r")
-
-    # ensure the file is in FASTA file format
-    header = file.readline()
-    if (header[0] == '>'):
-        print("in FASTA format")
-    else:
-        print("invalid format")
-        inputFile.close()
-        return
-
-    # open the file for output
-    out = "Cleaned_" + inputFile
+def cleanNT(file):
+    # open the input file for reading & output for writing
+    inputFile = open(file, "r")
+    out = "Cleaned_" + file + ".txt"
     output = open(out, 'w')
 
     # read the entire file, and close.
     entireText = inputFile.read()
-    file.close()
+    inputFile.close()
 
-    # get rid of newline characters.
-    entireText = entireText.replace("\r", "")
-    entireText = entireText.replace("\n", "")
+    # ensure the file is in FASTA file format
+    if (entireText[0] == '>'):
+        print("in FASTA format")
+    else:
+        print("invalid format")
+        output.close()
+        return
 
     # variables to keep track of the current and next
     # occurance of the '>' character
     pos1 = 0
     pos2 = 0
-    done = False
+
+    start = 0
+    end = 0
     
     # For all of the sequences in the .nt file:
     #   - Search for the first and last 5 consecutive
     #      nucleotide subsequence and trim the sequence
     #   - Write the subsequence to the text file
-    while(not done):
+    while(True):
 
-        # Find the next subsequence to clean
+        # Find the sequence until the next '>' char
         pos1 = pos2
-        pos2 = entireText.find(">", pos1 + 1)
+        pos2 = entireText.find('>', pos1 + 1)
 
-        # Check if we're cleaning the last sequence
-        if(pos2 == -1):
-            done = True
-            pos2 = len(sequence)
+        # If we've gone to the end of the text, exit.
+        if(pos2 < pos1):
+            break
 
-        # Get the sequence
-        sequence = entireText[pos1:pos2]
-
-        # Clean the front of sequence
-        for i in range(len(sequence)):
-            if( "N" not in sequence[i:i+5]):
-                start = i
-                break
-        # Then clean the back
-        for i in range(len(sequence), 0, -1):
-            if("N" not in sequence[i-5, i]):
-                end = i
-                break
+        # Get the cleaned and nicely formatted sequence
+        sequence = cleaner(entireText[pos1:pos2])
+##        print(sequence) # for debugging
+            
 
         # Write the cleaned sequence to the output file
-        output.write(string)
+        output.write(sequence)
         output.write("\r\n")
 
     # close and exit
     output.close()
+    print("done cleaning.")
+
+
+
+def cleaner(sequence):
+    # Write the '> Line' to the output file
+    endFirstLine = sequence.find("\n", 1)
+    firstPart = sequence[:endFirstLine]
+
+    sequence = sequence[endFirstLine:].replace("\r", "").replace("\n","")
+    
+    # Clean the front of sequence
+    for i in range(len(sequence)):
+        if( 'N' not in sequence[i:i+5]):
+            start = i
+            break
+        
+    # Then clean the back
+    for j in range(len(sequence), endFirstLine, -1):
+        if( 'N' not in sequence[j-5: j]):
+            end = j
+            break
+
+    sequence = makeNice(sequence[start:end])
+
+    return firstPart + "\n" + sequence
+
+
+# This function takes a long string and it returns
+# a string that has 60 characters per line.
+def makeNice(sequence):
+    output = ""
+    for i in range(0, len(sequence), 60):
+        output += sequence[i:i+60] + "\n"
+
+    return output
+
+#####################
+#### Run the code ###
+cleanNT("Micro-Seq.nt")
+
